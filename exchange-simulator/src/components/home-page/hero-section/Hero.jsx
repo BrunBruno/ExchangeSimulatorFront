4d20a7e6645eb211c433f2070ea87065d23f762d
BrunBruno <brunomaslon@gmail.com>
2023-07-05 16:39:25 +0200

    Add Hero

--- exchange-simulator/src/components/home-page/hero-section/Hero.jsx
+++ exchange-simulator/src/components/home-page/hero-section/Hero.jsx
@@ -1,5 +1,25 @@
+import classes from "./Hero.module.scss";
+
 function Hero() {
-  return;
+  return (
+    <div className={classes.hero}>
+      <div className={classes["hero__content"]}>
+        <div className={classes["hero__content__into"]}>
+          <h1>
+            Welcome to <br /> Exchnage Simulatore
+          </h1>
+          <h2>
+            Immerse yourself in the dynamic realm of financial markets through
+            our exchange simulator game webpage, where you can test your trading
+            strategies, learn new techniques, and compete with fellow virtual
+            traders.
+          </h2>
+        </div>
+      </div>
+
+      <div className={classes["hero__intro_figure"]}>a</div>
+    </div>
+  );
 }
 
 export default Hero;
