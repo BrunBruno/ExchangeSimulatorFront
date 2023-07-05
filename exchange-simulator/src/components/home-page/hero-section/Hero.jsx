9d8f75f8d7411ba3fd43d60fa45e63e59fc3decc
BrunBruno <brunomaslon@gmail.com>
2023-07-05 21:45:26 +0200

    Add figure

--- exchange-simulator/src/components/home-page/hero-section/Hero.jsx
+++ exchange-simulator/src/components/home-page/hero-section/Hero.jsx
@@ -1,10 +1,47 @@
 import classes from "./Hero.module.scss";
 
+import { useEffect, useRef } from "react";
+
 function Hero() {
+  const rotations = [
+    "",
+    "rotateX(90deg)",
+    "rotateX(-90deg)",
+    "rotateY(-90deg)",
+    "rotateY(90deg)",
+    "rotateY(180deg)",
+  ];
+  const figureRef = useRef(null);
+  const handleResize = () => {
+    if (figureRef && figureRef.current) {
+      const figure = figureRef.current;
+      figure.childNodes.forEach((d, i) => {
+        const transformSize = figure.offsetWidth / 2;
+
+        d.style.transform = `${rotations[i]} translateZ(${
+          transformSize * 1.5
+        }px)`;
+        d.childNodes.forEach((p, i) => {
+          if (i !== 0 && i !== d.childNodes.length - 1) {
+            p.style.transform = `${rotations[i]} translateZ(${transformSize}px)`;
+          }
+        });
+      });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={classes.hero}>
       <div className={classes["hero__content"]}>
-        <div className={classes["hero__content__into"]}>
+        <div className={classes["hero__content__intro"]}>
           <h1>
             Welcome to <br /> Exchnage Simulatore
           </h1>
@@ -17,7 +54,58 @@ function Hero() {
         </div>
       </div>
 
-      <div className={classes["hero__intro_figure"]}>a</div>
+      <div className={classes["hero__f-container"]}>
+        <div ref={figureRef} className={classes["hero__f-container__figure"]}>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
