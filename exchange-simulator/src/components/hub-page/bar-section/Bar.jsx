dadec5b64a2c92782f7c332c23d620f215b77ba2
BrunBruno <brunomaslon@gmail.com>
2023-07-09 23:21:57 +0200

    Add bar icons

--- exchange-simulator/src/components/hub-page/bar-section/Bar.jsx
+++ exchange-simulator/src/components/hub-page/bar-section/Bar.jsx
@@ -1,13 +1,30 @@
 import classes from "./Bar.module.scss";
+import BarIcons from "./BarIcons";
 
 function Bar() {
   return (
     <div className={classes.bar}>
       <ul>
-        <li>Create Game</li>
+        <li>
+          <BarIcons name="plus" />
+          <span>Create Game</span>
+        </li>
+        <li>
+          <BarIcons name="friend" />
+          <span>Invite Friend</span>
+        </li>
+        <li>
+          <BarIcons name="account" />
+          <span>Account Details</span>
+        </li>
+        <li>
+          <BarIcons name="ranking" />
+          <span>See Ranking</span>
+        </li>
+        {/* <li>Create Game</li>
         <li>Invite Friend</li>
         <li>Account Details</li>
-        <li>See Ranking</li>
+        <li>See Ranking</li> */}
       </ul>
     </div>
   );
