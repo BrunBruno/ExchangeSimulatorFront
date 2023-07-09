55393dd26c4e97d668611f802fa6c5faaf6d9d57
BrunBruno <brunomaslon@gmail.com>
2023-07-09 19:10:01 +0200

    Add paggination

--- exchange-simulator/src/components/hub-page/games-section/Crad.jsx
+++ exchange-simulator/src/components/hub-page/games-section/Crad.jsx
@@ -1,14 +1,76 @@
+import { useState } from "react";
 import classes from "./Games.module.scss";
 
 function Card(props) {
-  let currentPage = 1;
-  let totalPages = 5;
+  const [currentPage, setCurrentPage] = useState(1);
+  let totalPages = 50;
+  const buttonsToShow = 3;
   const pageNumbers = Array.from(
     { length: totalPages },
     (_, index) => index + 1
   );
 
-  const onPageChange = () => {};
+  const onPageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const renderPageButtons = () => {
+    if (totalPages <= buttonsToShow) {
+      return pageNumbers.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => onPageChange(pageNumber)}
+          disabled={currentPage === pageNumber}
+          className={currentPage === pageNumber ? classes.currentPage : ""}
+        >
+          {pageNumber}
+        </button>
+      ));
+    } else {
+      const middlePage = Math.floor(buttonsToShow / 2);
+      let startPage = currentPage - middlePage;
+      let endPage = currentPage + middlePage;
+
+      if (startPage <= 1) {
+        startPage = 1;
+        endPage = buttonsToShow + 2;
+      } else if (endPage >= totalPages) {
+        endPage = totalPages;
+        startPage = totalPages - buttonsToShow - 1;
+      }
+
+      return (
+        <>
+          {startPage > 1 && (
+            <>
+              <button onClick={() => onPageChange(1)}>1</button>
+              {startPage >= 2 && <p>...</p>}
+            </>
+          )}
+
+          {pageNumbers.slice(startPage - 1, endPage).map((pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => onPageChange(pageNumber)}
+              disabled={currentPage === pageNumber}
+              className={currentPage === pageNumber ? classes.currentPage : ""}
+            >
+              {pageNumber}
+            </button>
+          ))}
+
+          {endPage < totalPages && (
+            <>
+              {endPage <= totalPages - 1 && <p>...</p>}
+              <button onClick={() => onPageChange(totalPages)}>
+                {totalPages}
+              </button>
+            </>
+          )}
+        </>
+      );
+    }
+  };
 
   return (
     <div className={`${classes["games__card"]} ${classes["column-fit"]}`}>
@@ -18,54 +80,27 @@ function Card(props) {
         <button>Search</button>
       </form>
       <ul>
-        <li>Game 1</li>
-        <li>Game 2</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
-        <li>Game 3</li>
+        <li>Game</li>
+        <li>Game</li>
+        <li>Game</li>
+        <li>Game</li>
+        <li>Game</li>
       </ul>
       <div className={classes.pagination}>
         <button
           disabled={currentPage === 1}
           onClick={() => onPageChange(currentPage - 1)}
+          className={classes.currentPage}
         >
           {"<"}
         </button>
 
-        {pageNumbers.map((pageNumber, index) => {
-          if (
-            pageNumber === 1 ||
-            pageNumber === totalPages ||
-            (pageNumber >= currentPage - 2 && pageNumber <= currentPage + 2)
-          ) {
-            return (
-              <button
-                key={pageNumber}
-                onClick={() => onPageChange(pageNumber)}
-                disabled={currentPage === pageNumber}
-              >
-                {pageNumber}
-              </button>
-            );
-          } else if (
-            (pageNumber === currentPage - 3 && currentPage !== 4) ||
-            (pageNumber === currentPage + 3 && currentPage !== totalPages - 3)
-          ) {
-            return <span key={pageNumber}>...</span>;
-          }
-          return null;
-        })}
+        {renderPageButtons()}
 
         <button
           disabled={currentPage === totalPages}
           onClick={() => onPageChange(currentPage + 1)}
+          className={classes.currentPage}
         >
           {">"}
         </button>
