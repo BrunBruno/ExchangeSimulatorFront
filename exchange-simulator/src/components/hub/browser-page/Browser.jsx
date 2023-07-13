3f8a9ec360addbbf8e642b8313a113d7f4fdc658
BrunBruno <brunomaslon@gmail.com>
2023-07-14 01:22:57 +0200

    Change browswer look

--- exchange-simulator/src/components/hub/browser-page/Browser.jsx
+++ exchange-simulator/src/components/hub/browser-page/Browser.jsx
@@ -1,19 +1,187 @@
 import { useLocation } from "react-router-dom";
-import classes from "./Browser.module.scss";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
+import classes from "./Browser.module.scss";
+import cardclasses from "./Card.module.scss";
+
+import baseUrl from "../../Shared/Url";
 import Card from "./Card";
 import Details from "./Details";
 import Header from "../header-shared/Header";
 
 function Browser() {
+  const containerRef = useRef(null);
+  const cardsRefs = useRef([]);
   const location = useLocation();
 
+  const [gameList, setGameList] = useState([]);
+  const [filteredGameList, setFilteredGameList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const onLoadGames = async () => {
+      try {
+        const games = await axios.get(`${baseUrl}/game/available-games`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+
+        setGameList(games.data);
+      } catch (err) {
+        //
+        console.log(err);
+      }
+    };
+
+    onLoadGames();
+    setTimeout(() => {
+      onPageChange(1);
+    }, 100);
+  }, []);
+
+  useEffect(() => {
+    setFilteredGameList(gameList);
+  }, [gameList]);
+
+  const gamesPerPage = 6;
+  const indexOfLastGame = currentPage * gamesPerPage;
+  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+  const totalPages = Math.ceil(filteredGameList.length / gamesPerPage);
+  const currentGames = filteredGameList.slice(
+    indexOfFirstGame,
+    indexOfLastGame
+  );
+
+  const onPageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    setTimeout(() => {
+      cardsRefs.current.forEach((element) => {
+        if (element) {
+          element.classList.add(cardclasses.hidden);
+        }
+      });
+    }, 100);
+    setTimeout(() => {
+      cardsRefs.current.forEach((element, index) => {
+        setTimeout(() => {
+          if (element) {
+            element.classList.remove(cardclasses.hidden);
+          }
+        }, 50 * Math.floor(Math.random() * gamesPerPage) + 1);
+      });
+    }, 100);
+  };
+
+  const onFliterGames = (event) => {
+    const filteredGames = gameList.filter((game) =>
+      game.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setFilteredGameList(filteredGames);
+    onPageChange(1);
+  };
+
+  const renderPageButtons = (array) => {
+    const buttonsToShow = 3;
+    if (totalPages <= buttonsToShow) {
+      return array.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => onPageChange(pageNumber)}
+          disabled={currentPage === pageNumber}
+          className={currentPage === pageNumber ? classes.currentPage : ""}
+        >
+          {pageNumber}
+        </button>
+      ));
+    } else {
+      const middlePage = Math.floor(buttonsToShow / 2);
+      let startPage = currentPage - middlePage;
+      let endPage = currentPage + middlePage;
+
+      if (startPage <= 1) {
+        startPage = 1;
+        endPage = buttonsToShow + 2;
+      } else if (endPage >= totalPages) {
+        endPage = totalPages;
+        startPage = totalPages - buttonsToShow - 1;
+      }
+
+      return (
+        <>
+          {startPage > 1 && (
+            <>
+              <button onClick={() => onPageChange(1)}>1</button>
+              {startPage >= 2 && <p>...</p>}
+            </>
+          )}
+
+          {array.slice(startPage - 1, endPage).map((pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => onPageChange(pageNumber)}
+              disabled={currentPage === pageNumber}
+              className={currentPage === pageNumber ? classes.currentPage : ""}
+            >
+              {pageNumber}
+            </button>
+          ))}
+
+          {endPage < totalPages && (
+            <>
+              {endPage <= totalPages - 1 && <p>...</p>}
+              <button onClick={() => onPageChange(totalPages)}>
+                {totalPages}
+              </button>
+            </>
+          )}
+        </>
+      );
+    }
+  };
+
   return (
-    <div>
-      <Header />
+    <div ref={containerRef}>
+      <Header containerRef={containerRef} />
       <div className={classes.browser}>
-        <Card title={location.state.title} />
-        <Details />
+        <div className={classes["browser__search"]}>
+          <h2>{location.state.title}</h2>
+          <input type="text" placeholder="Search" onChange={onFliterGames} />
+        </div>
+        <div className={classes["browser__cards"]}>
+          <ul>
+            {currentGames.map((game, index) => (
+              <Card
+                key={index}
+                name={game.name}
+                owner={game.ownerName}
+                cardRef={(el) => (cardsRefs.current[index] = el)}
+              />
+            ))}
+          </ul>
+          <div className={classes.pagination}>
+            <button
+              disabled={currentPage === 1}
+              onClick={() => onPageChange(currentPage - 1)}
+              className={classes.currentPage}
+            >
+              {"<"}
+            </button>
+
+            {renderPageButtons(
+              Array.from({ length: totalPages }, (_, index) => index + 1)
+            )}
+
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => onPageChange(currentPage + 1)}
+              className={classes.currentPage}
+            >
+              {">"}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- exchange-simulator/src/components/hub/browser-page/Card.jsx
+++ exchange-simulator/src/components/hub/browser-page/Card.jsx
@@ -1,154 +1,36 @@
 import { useEffect, useState } from "react";
 
 import classes from "./Card.module.scss";
-import axios from "axios";
-import baseUrl from "../../Shared/Url";
 
 function Card(props) {
-  const [gameList, setGameList] = useState([]);
-  const [filteredGameList, setFilteredGameList] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [randomStyle, setRandomStyle] = useState("");
 
   useEffect(() => {
-    const onLoadGames = async () => {
-      try {
-        const games = await axios.get(`${baseUrl}/game/available-games`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
-
-        setGameList(games.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    onLoadGames();
-  }, []);
-
-  useEffect(() => {
-    setFilteredGameList(gameList);
-  }, [gameList]);
-
-  const gamesPerPage = 10;
-  const buttonsToShow = 3;
-  const indexOfLastGame = currentPage * gamesPerPage;
-  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
-  const currentGames = filteredGameList.slice(
-    indexOfFirstGame,
-    indexOfLastGame
-  );
-  const totalPages = Math.ceil(filteredGameList.length / gamesPerPage);
-
-  const onPageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const onFliterGames = (event) => {
-    const filteredGames = gameList.filter((game) =>
-      game.name.toLowerCase().includes(event.target.value.toLowerCase())
-    );
-    setFilteredGameList(filteredGames);
-  };
-
-  const renderPageButtons = (array) => {
-    if (totalPages <= buttonsToShow) {
-      return array.map((pageNumber) => (
-        <button
-          key={pageNumber}
-          onClick={() => onPageChange(pageNumber)}
-          disabled={currentPage === pageNumber}
-          className={currentPage === pageNumber ? classes.currentPage : ""}
-        >
-          {pageNumber}
-        </button>
-      ));
-    } else {
-      const middlePage = Math.floor(buttonsToShow / 2);
-      let startPage = currentPage - middlePage;
-      let endPage = currentPage + middlePage;
-
-      if (startPage <= 1) {
-        startPage = 1;
-        endPage = buttonsToShow + 2;
-      } else if (endPage >= totalPages) {
-        endPage = totalPages;
-        startPage = totalPages - buttonsToShow - 1;
-      }
-
-      return (
-        <>
-          {startPage > 1 && (
-            <>
-              <button onClick={() => onPageChange(1)}>1</button>
-              {startPage >= 2 && <p>...</p>}
-            </>
-          )}
-
-          {array.slice(startPage - 1, endPage).map((pageNumber) => (
-            <button
-              key={pageNumber}
-              onClick={() => onPageChange(pageNumber)}
-              disabled={currentPage === pageNumber}
-              className={currentPage === pageNumber ? classes.currentPage : ""}
-            >
-              {pageNumber}
-            </button>
-          ))}
-
-          {endPage < totalPages && (
-            <>
-              {endPage <= totalPages - 1 && <p>...</p>}
-              <button onClick={() => onPageChange(totalPages)}>
-                {totalPages}
-              </button>
-            </>
-          )}
-        </>
-      );
-    }
-  };
-
+    const styles = [
+      "content-style-1",
+      "content-style-2",
+      "content-style-3",
+      "content-style-4",
+      "content-style-5",
+      "content-style-6",
+      "content-style-7",
+      "content-style-8",
+      "content-style-9",
+    ];
+    const randomIndex = Math.floor(Math.random() * styles.length);
+    setRandomStyle(classes[styles[randomIndex]]);
+  });
   return (
-    <div className={classes.card}>
-      <h2>{props.title}</h2>
-      <form className={classes["form-search"]}>
-        <input type="text" placeholder="Search" onChange={onFliterGames} />
-      </form>
-      <ul>
-        {filteredGameList.map((game, index) => (
-          <li key={index}>
-            <div>
-              {indexOfFirstGame + index + 1}. {game.name}
-            </div>
-            <div>
-              <button>Join Now</button>
-              <button>Check Details</button>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <div className={classes.pagination}>
-        <button
-          disabled={currentPage === 1}
-          onClick={() => onPageChange(currentPage - 1)}
-          className={classes.currentPage}
-        >
-          {"<"}
-        </button>
-
-        {renderPageButtons(
-          Array.from({ length: totalPages }, (_, index) => index + 1)
-        )}
-
-        <button
-          disabled={currentPage === totalPages}
-          onClick={() => onPageChange(currentPage + 1)}
-          className={classes.currentPage}
-        >
-          {">"}
-        </button>
+    <div ref={props.cardRef} className={classes.card}>
+      <div className={`${classes["card__content"]} ${randomStyle}`}>
+        <div className={classes.text}>
+          <h2>{props.name}</h2>
+          <p>by {props.owner}</p>
+        </div>
+        <div className={classes.buttons}>
+          <button>Join</button>
+          <button>Select</button>
+        </div>
       </div>
     </div>
   );
